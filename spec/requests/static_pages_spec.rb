746b1c8f0f050edb3ee5653c4273cd241b36c8dc
Bruno Andrade <brunomello.andrade@gmail.com>
2013-09-17 20:00:12 -0300

    Layour e rotas prontos

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,27 +4,27 @@ describe "Static Pages" do
 
   describe "Home page" do
 	it "Quero ver o conteudo 'Me Ajuda Nisso'" do
-    	visit '/static_pages/home'
+    	visit root_path
     	page.should have_content('Me Ajuda Nisso')
     end	
   end
   
   describe "Help Page" do
   	it "Quero ver o conteudo 'Ajuda'"do
-  		visit '/static_pages/help'
+  		visit help_path
   		page.should have_content('Ajuda')
   	end
   end
   
   describe "About page" do
   	it "Quero ver o conteudo 'Sobre'" do
-  		visit '/static_pages/about'
+  		visit about_path
   		page.should have_content('Sobre')
   	end
   end
   describe "Contact page" do
   	it "Quero ver no h1 'Contato'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_content('Contato')
   	end
   end 
